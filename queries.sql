03fb3d7e8fe0362481bd9eb659fc2c79148df286
Cuongcoder1107 <Cuongnguyenthe1107@gmail.com>
2024-12-11 15:28:33 +0700

    update

--- queries.sql
+++ queries.sql
@@ -6,6 +6,6 @@ SELECT u.Id, u.First_Name AS NAME,
 (SELECT SUM(c.Total_Books) FROM cart c WHERE c.User_Id = u.Id) FROM user u;
 -- 7e. Query using group by and aggregate functions.
 -- Mỗi một tác giả đã viết ra bao nhiêu quyển sách?
-SELECT a.Author_Id, a.Name, COUNT(b.Book_Id) AS Total_Books FROM author a JOIN book b ON a.Book_Id = b.Book_Id GROUP BY a.Author_Id;
+SELECT a.Author_Id, a.Name, COUNT(b.Book_Id) AS Total_Books FROM author a JOIN book b ON a.Author_Id = b.Author_Id GROUP BY a.Author_Id;
 -- Mỗi một user đã review bao nhiêu bài
 SELECT u.Id, u.Last_Name AS Name, COUNT(r.Id) FROM user u JOIN review r ON u.Id = r.Customer_Id GROUP BY u.Id;
