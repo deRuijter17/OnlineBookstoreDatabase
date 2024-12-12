defbcd0d72f6f3db384f858fd0710e38421e189b
Hoàng Dương <hoangvanduong1359@gmail.com>
2024-12-12 16:22:53 +0700

    update

--- createDB.sql
+++ createDB.sql
@@ -1,6 +1,15 @@
 CREATE DATABASE IF NOT EXISTS OnlineBookstore;
 USE OnlineBookstore;
-
+DROP TABLE IF EXISTS Payment;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Cart;
+DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS Book;
+DROP TABLE IF EXISTS Author;
+DROP TABLE IF EXISTS Language;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS City;
+DROP TABLE IF EXISTS Country;
 CREATE TABLE IF NOT EXISTS Country
 (
     Id   INT PRIMARY KEY AUTO_INCREMENT,
@@ -42,11 +51,11 @@ CREATE TABLE IF NOT EXISTS Book
     Language         INT,
     Publication_Date DATE,
     Author_Id        INT,
-    Price            FLOAT                                                            NOT NULL,
+    Price            FLOAT                                                            NOT NULL CHECK ( Price >= 0 ),
     Image            BLOB,
     Edition          INT,
     Status           BOOLEAN                                                          NOT NULL,
-    Stock            INT                                                              NOT NULL,
+    Stock            INT                                                              NOT NULL CHECK ( Stock >= 0 ),
     Genre            ENUM ('Fiction', 'Non-Fiction', 'Science', 'Biography', 'Other') NOT NULL
 );
 
@@ -75,8 +84,8 @@ CREATE TABLE IF NOT EXISTS Cart
     Id          INT PRIMARY KEY AUTO_INCREMENT,
     User_Id     INT   NOT NULL,
     Book_Id     INT   NOT NULL,
-    Total_Books INT,
-    Total_Price FLOAT NOT NULL,
+    Total_Books INT check ( Total_Books >=0 ),
+    Total_Price FLOAT NOT NULL check ( Total_Price >=0 ),
     Discount    FLOAT
 );
 
@@ -84,7 +93,7 @@ CREATE TABLE IF NOT EXISTS Orders
 (
     Id           INT PRIMARY KEY AUTO_INCREMENT,
     Cart_Id      INT,
-    Quantity     INT                                                   NOT NULL,
+    Quantity     INT                                                   NOT NULL CHECK ( Quantity >= 0 ),
     Customer_Id  INT,
     Order_Date   DATE                                                  NOT NULL,
     Dest_Address VARCHAR(255)                                          NOT NULL,
