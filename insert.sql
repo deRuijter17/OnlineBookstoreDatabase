a33801a07fd800fa2089723fc28e3939315b4b93
deRuijter17 <vddung1900@gmail.com>
2024-12-12 16:40:14 +0700

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO Country (Id, Name) VALUES
+INSERT INTO Country (Country_Id, Name) VALUES
 (1, 'United States'),
 (2, 'United Kingdom'),
 (3, 'France'),
@@ -12,7 +12,7 @@ INSERT INTO City (Pincode, Name, Country) VALUES
 (4, 'Berlin', 4),
 (5, 'Shanghai', 5);
 
-INSERT INTO User (Id, First_Name, Last_Name, Email, Password, DOB, Address_Line1, City, Phone_Number, Role) VALUES
+INSERT INTO User (User_Id, First_Name, Last_Name, Email, Password, DOB, Address_Line1, City, Phone_Number, Role) VALUES
 (1, 'Alice', 'Smith', 'alicesmith01@gmail.com', 'Alice@123', '1990-01-01', '123 Elm St', 1, '1234567890', 'Customer'),
 (2, 'Bob', 'Johnson', 'bobjohnson22@gmail.com', 'Bob@456!', '1985-02-15', '456 Oak St', 2, '9876543211', 'Customer'),
 (3, 'Charlie', 'Brown', 'charliebrown36@gmail.com', 'Charlie#789$', '1975-03-30', '789 Pine St', 3, '1928374652', 'Customer'),
@@ -20,7 +20,7 @@ INSERT INTO User (Id, First_Name, Last_Name, Email, Password, DOB, Address_Line1
 (5, 'Eve', 'Davis', 'evedavis58@gmail.com', 'Eve#654$', '1995-07-15', '321 Cedar St', 1, '8273649584', 'Customer'),
 (6, 'Fan', 'Xiaoping', 'fxy1506@gmail.com', 'Fxy#jqkA', '2005-06-15', '17 Kuomintang St', 5, '8271239584', 'Customer');
 
-INSERT INTO Language (Id, Name) VALUES
+INSERT INTO Language (Language_Id, Name) VALUES
 (1, 'English'),
 (2, 'Spanish'),
 (3, 'French'),
@@ -48,14 +48,14 @@ INSERT INTO Book (Book_Id, Title, Language, Publication_Date, Author_Id, Price, 
 (9, 'Faust', 4, '1808-01-01', 6, 30.00, 1, 20, TRUE, 'Biography'),
 (10, 'Dream of the Red Chamber', 5, '1791-01-01', 7, 35.99, 1, 40, TRUE, 'Biography');
 
-INSERT INTO Review (Id, Customer_Id, Book_Id, Review, Date) VALUES
+INSERT INTO Review (Review_Id, Customer_Id, Book_Id, Review, Date) VALUES
 (1, 1, 1, 'Great book! A magical journey that introduces readers to the enchanting world of Hogwarts.', '2024-12-05'),
 (2, 1, 5, 'A chilling look into a dystopian world, must read for 1984 fans.', '2024-12-07'),
 (3, 3, 6, 'A powerful political allegory that critiques the corruption of power through a group of farm animals.', '2024-12-15'),
 (4, 3, 7, 'A timeless romance with witty dialogue and sharp social commentary, highly recommended it!', '2024-12-18'),
 (5, 5, 9, 'A deeply philosophical work that delves into human desires, morality, and the quest for meaning.', '2024-12-19');
 
-INSERT INTO Cart (Id, User_Id, Book_Id, Total_Books, Total_Price, Discount) VALUES
+INSERT INTO Cart (Cart_Id, User_Id, Book_Id, Total_Books, Total_Price, Discount) VALUES
 (1, 1, 1, 1, 29.99, 0.00),
 (2, 1, 5, 2, 41.98, 2.00),
 (3, 2, 3, 3, 77.97, 5.00),
@@ -65,17 +65,35 @@ INSERT INTO Cart (Id, User_Id, Book_Id, Total_Books, Total_Price, Discount) VALU
 (7, 5, 9, 1, 30.00, 0.00),
 (8, 6, 10, 3, 107.97, 3.00);
 
-INSERT INTO Orders (Id, Cart_Id, Quantity, Customer_Id, Order_Date, Dest_Address, Status, Payment) VALUES
-(1, 1, 1, 1, '2024-11-25', '123 Elm St, New York', 'Shipped', TRUE),
-(2, 2, 2, 1, '2024-11-28', '123 Elm St, New York', 'Shipped', TRUE),
-(3, 3, 3, 2, '2024-12-03', '456 Oak St, London', 'Pending', FALSE),
-(4, 4, 1, 3, '2024-12-07', '789 Pine St, Paris', 'Shipped', TRUE),
-(5, 5, 2, 3, '2024-12-11', '789 Pine St, Paris', 'Shipped', TRUE),
-(6, 6, 1, 4, '2024-12-12', '654 Maple St, Berlin', 'Delivered', TRUE),
-(7, 7, 1, 5, '2024-12-13', '321 Cedar St, New York', 'Shipped', TRUE),
-(8, 8, 3, 6, '2024-12-15', '17 Kuomintang St, Shanghai', 'Delivered', TRUE);
+INSERT INTO Cart (Cart_Id, User_Id, Total_Books, Total_Price, Discount) VALUES
+(1, 4, 1, 29.99, 0.00),
+(2, 2, 2, 41.98, 2.00),
+(3, 1, 3, 77.97, 5.00),
+(4, 5, 1, 15.99, 0.00),
+(5, 6, 2, 37.98, 1.00),
+(6, 3, 3, 107.97, 3.00);
 
-INSERT INTO Payment (Id, Customer_Id, Order_Id, Status, Payment_Method) VALUES
+INSERT INTO Wishlist (Cart_Id, Book_Id, Number_Of_Books) VALUES
+(1, 8, 1),
+(2, 3, 3),
+(3, 1, 1),
+(3, 5, 2),
+(4, 9, 1),
+(5, 10,3),
+(6, 6, 1),
+(6, 7, 2);
+
+INSERT INTO Orders (Order_Id, Cart_Id, Customer_Id, Order_Date, Dest_Address, Status, Payment) VALUES
+(1, 1, 1, '2024-11-25', '123 Elm St, New York', 'Shipped', TRUE),
+(2, 2, 1, '2024-11-28', '123 Elm St, New York', 'Shipped', TRUE),
+(3, 3, 2, '2024-12-03', '456 Oak St, London', 'Pending', FALSE),
+(4, 4, 3, '2024-12-07', '789 Pine St, Paris', 'Shipped', TRUE),
+(5, 5, 3, '2024-12-11', '789 Pine St, Paris', 'Shipped', TRUE),
+(6, 6, 4, '2024-12-12', '654 Maple St, Berlin', 'Delivered', TRUE),
+(7, 7, 5, '2024-12-13', '321 Cedar St, New York', 'Shipped', TRUE),
+(8, 8, 6, '2024-12-15', '17 Kuomintang St, Shanghai', 'Delivered', TRUE);
+
+INSERT INTO Payment (Payment_Id, Customer_Id, Order_Id, Status, Payment_Method) VALUES
 (1, 1, 1, 'Success', 'Credit Card'),
 (2, 1, 2, 'Success', 'Debit Card'),
 (3, 2, 3, 'Pending', 'Cash on Delivery'),
